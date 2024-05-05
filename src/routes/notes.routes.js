a82dba04309f214a3418a3fbe1c3075810fc383d
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-05 19:26:17 -0300

    Cria rota show para notes

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -5,6 +5,7 @@ const notesRoutes = Router()
 const NotesController = require("../controllers/NotesController")
 const notesController = new NotesController()
 
+notesRoutes.get("/:id", notesController.show)
 notesRoutes.post("/:user_id", notesController.create)
 
 module.exports = notesRoutes
