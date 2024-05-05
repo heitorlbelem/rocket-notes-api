628df23db5ef5ffaacd611171dbeadbdf4e20593
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-05 16:49:46 -0300

    Remove middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,14 +5,6 @@ const usersRoutes = Router()
 const UsersController = require("../controllers/UsersController")
 const usersController = new UsersController()
 
-function myMiddleware(request, response, next) {
-  if(!request.body.isAdmin) {
-    return response.status(401).json({ message: 'user unauthorized' })
-  }
-
-  next()
-}
-
-usersRoutes.post("/", myMiddleware, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
