5e0344f68948a21c97b0bf433b1adff0f9f2b909
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-06 09:28:48 -0300

    Remove console.log

--- src/controllers/TagsController.js
+++ src/controllers/TagsController.js
@@ -3,7 +3,6 @@ const knex = require("../database/knex")
 class NotesController {
   async index(request, response) {
     const { user_id } = request.params
-    console.log('oi')
 
     const tags = await knex("tags").where({ user_id })
 
