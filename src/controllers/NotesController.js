432f7b5785139053de5962cdb833044b666e5a22
Heitor Belem <heitorlbelem98@gmail.com>
2024-05-06 00:47:01 -0300

    Adiciona index e delete para notes controller

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -1,6 +1,18 @@
 const knex = require("../database/knex")
+const AppError = require("../utils/AppError")
 
 class NotesController {
+  async index(request, response) {
+    const { title, user_id } = request.query
+
+    const notes = await knex("notes")
+      .whereLike("title", `%${title}%`)
+      .where({ user_id })
+      .orderBy("title")
+
+    return response.json(notes)
+  }
+
   async create(request, response) {
     const { title, description, tags, links } = request.body
     const { user_id } = request.params
@@ -28,9 +40,9 @@ class NotesController {
   }
 
   async show(request, response) {
-    const { id } = request.params;
+    const { id } = request.params
 
-    const note = await knex("notes").where({ id }).first();
+    const note = await knex("notes").where({ id }).first()
     const tags = await knex("tags").where({ note_id: note.id }).orderBy("name")
     const links = await knex("links").where({ note_id: note.id}).orderBy("created_at")
 
@@ -39,6 +51,19 @@ class NotesController {
       tags,
       links
     })
+  }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    const checkNoteExists = await knex("notes").where({ id }).first()
+    if(!checkNoteExists) {
+      throw new AppError("Nota não foi encontrada", 404)
+    }
+
+    await knex("notes").where({ id }).delete()
+
+    return response.status(204).json()
   }
 }
 
--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -5,7 +5,9 @@ const notesRoutes = Router()
 const NotesController = require("../controllers/NotesController")
 const notesController = new NotesController()
 
+notesRoutes.get("/", notesController.index)
 notesRoutes.get("/:id", notesController.show)
 notesRoutes.post("/:user_id", notesController.create)
+notesRoutes.delete("/:id", notesController.delete)
 
 module.exports = notesRoutes
